b6b3cd4654b0da1a27e4a7b0614e3de70c1bb64d
Zippylicious <zakthompson97@gmail.com>
2021-10-26 14:16:19 -0500

    Basic design for scheduling tab done.

--- app/src/main/java/com/example/messagescheduler/ui/main/MessageSchedulerFragment.java
+++ app/src/main/java/com/example/messagescheduler/ui/main/MessageSchedulerFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +18,21 @@ public class MessageSchedulerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_scheduler, viewGroup, false);
+
+        View v = inflater.inflate(R.layout.fragment_scheduler, viewGroup, false);
+
+        //Create dropdown menu for selecting which app to schedule messages through
+        Spinner appsDropdown = v.findViewById(R.id.messagingAppSelector);
+        String[] apps = new String[]{"WhatsApp"};
+        ArrayAdapter<String> appsAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_dropdown_item, apps);
+        appsDropdown.setAdapter(appsAdapter);
+
+        //Create dropdown menu for selecting the frequency for the scheduled message
+        Spinner frequencyDropdown = v.findViewById(R.id.frequencySelector);
+        String[] frequencies = new String[]{"Daily"};
+        ArrayAdapter<String> frequencyAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_dropdown_item, frequencies);
+        frequencyDropdown.setAdapter(frequencyAdapter);
+
+        return v;
     }
 }

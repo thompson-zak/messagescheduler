86229a791720515774c8ef8ef94e0a18676887b2
Zippylicious <zakthompson97@gmail.com>
2021-11-05 20:37:07 -0700

    Scheduled message list now updates on new message input in other tab

--- app/src/main/java/com/example/messagescheduler/ui/main/ScheduledMessagesFragment.java
+++ app/src/main/java/com/example/messagescheduler/ui/main/ScheduledMessagesFragment.java
@@ -1,6 +1,7 @@
 package com.example.messagescheduler.ui.main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,20 +20,35 @@ import java.util.List;
  */
 public class ScheduledMessagesFragment extends Fragment {
 
+    MessagesAdapter adapter;
+    SchedulerDatabase db;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
 
         View v = inflater.inflate(R.layout.fragment_scheduled, viewGroup, false);
 
         //Fetch list of scheduled messages from room DB
-        SchedulerDatabase db = SchedulerDatabase.getInstance(getActivity());
+        db = SchedulerDatabase.getInstance(getActivity());
         List<ScheduledMessage> messages = db.getScheduledMessageDao().getAll();
 
         //Create and populate list view using custom messages adapter
-        MessagesAdapter adapter = new MessagesAdapter(this.getContext(), messages);
+        adapter = new MessagesAdapter(this.getContext(), messages);
         ListView listView = v.findViewById(R.id.messages_list_view);
         listView.setAdapter(adapter);
 
         return v;
+    }
+
+    @Override
+    public void setMenuVisibility(boolean menuVisible) {
+        super.setMenuVisibility(menuVisible);
+        if(db != null) {
+            Log.v("Set Menu Visibility", "Updating scheduled messages with setMenuVisibility");
+            List<ScheduledMessage> messages = db.getScheduledMessageDao().getAll();
+            adapter.clear();
+            adapter.addAll(messages);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
